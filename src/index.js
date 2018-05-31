0f1d1b1f7af6005ce5bdfcd3ff88aeef722e8830
Arek Żelechowski <aze@zonith.com>
2018-05-31 17:05:08 +0200

    Bump obfuscator version. Add ability to use custom version of obfuscator by specifying `options.obfuscator`.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import JavaScriptObfuscator from 'javascript-obfuscator';
-
 export default function javascriptObfuscator(options = {}) {
 	return {
 		name: 'javascript-obfuscator',
 
 		transformBundle(code) {
-			const obfuscationResult = JavaScriptObfuscator.obfuscate(code, options);
+			const obfuscator = options.obfuscator ? options.obfuscator : require('javascript-obfuscator');
+			const obfuscationResult = obfuscator.obfuscate(code, options);
 			let result = {code: obfuscationResult.getObfuscatedCode()};
 
 			if (options.sourceMap && options.sourceMapMode !== 'inline') {
